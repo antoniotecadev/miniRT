947261ced5cd99370a059f1a76fdacd8d7d70199
antoniotecadev <antonioteca@hotmail.com>
2025-05-21 11:35:49 +0100

    get ray direction: convert coordinate pixel windows (x, y) in vector direction of camera. Created a test ray trace with color gradient, add color pixel of image mlx

--- source/main.c
+++ source/main.c
@@ -25,15 +25,16 @@ int	main(int argc, char **argv)
 	}
 	file = argv[1];
 	read_scene(file, &scene);
+	img.scene = scene;
 	img.mlx = mlx_init();
 	img.win = mlx_new_window(img.mlx, WIDTH, HEIGHT, "miniRT");
 	img.img = mlx_new_image(img.mlx, WIDTH, HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
 			&img.line_length, &img.endian);
+	render_scene(&scene, &img);
 	mlx_put_image_to_window(img.mlx, img.win, img.img, 0, 0);
 	mlx_hook(img.win, 2, 1L << 0, events_press_esc, &img);
 	mlx_hook(img.win, 17, 0L, close_window_x, &img);
-	img.scene = scene;
 	t_object_list *current = scene.object_list;
 	while (current != NULL)
 	{
